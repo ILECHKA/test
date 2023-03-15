040e95a8fd73e83dff21609b303bfd2d82d9ca60
ILLIA <dostogoj@gmail.com>
2023-03-15 16:00:06 +0100

    history+hidden

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 function createElement(anime){
   const list = document.createElement('ul');
   list.classList.add('list');
+  list.classList.add('list__random');
   list.innerHTML = `<li class="list__item"><img src="${anime.image}"></li>
   <li class="list__item">${anime.title}</li>
   <li class="list__item">${anime.titleJapanese}</li>
   <li class="list__item">${anime.synopsis}</li>
   <li class="list__item"><a class="link" href="${anime.url}" target="_blank">MyAnimeList</a></li>
-  <li class="list__item list_button"></li>
   `
   const content = document.querySelector('.content');
   content.prepend(list);
@@ -15,9 +15,12 @@ function createElement(anime){
 const button = document.querySelector('.button');
 button.addEventListener('click', () => {
   getRandomAnime();
+  clear();
+  createHistory();
+  showButton.classList.remove('hidden');
 });
 
-
+const historyData = [];
 function animeObject (json) {
   const title = json.title;
   const image = json.images.jpg.image_url
@@ -25,8 +28,36 @@ function animeObject (json) {
   const synopsis = json.synopsis
   const url = json.url
   const anime = {title, image, titleJapanese, synopsis, url};
+  console.log(anime);
+  historyData.push(anime);
+  console.log(historyData)
   createElement(anime);
+  
 }
+
+
+function createHistory (){
+  const list = document.createElement('ul');
+  list.classList.add('list');
+  list.classList.add('list_history');
+  let data = historyData[historyData.length - 1];
+  list.innerHTML = `
+  <li class="list__item history_item">${data.title}</li>
+  <li class="list__item history_item"><a class="link" href="${data.url}" target="_blank">MyAnimeList</a></li>
+  `;
+  const history = document.querySelector('.history');
+  history.prepend(list);
+ 
+}
+
+
+const showButton = document.querySelector('.show_history');
+const history = document.querySelector('.history')
+showButton.addEventListener('click', () => {
+  history.classList.toggle('hidden');
+  const arrow = document.getElementById('Capa_1');
+  arrow.classList.toggle('capa_1')
+});
 
 async function getRandomAnime() {
   try{
@@ -36,13 +67,13 @@ async function getRandomAnime() {
   }catch(error){
     console.log(error.message);
   }
-}
-getRandomAnime();
+};
 
-//const getRandomThen = () => {
-  //fetch("https://api.jikan.moe/v4/random/anime")
-   // .then((data) => data.json())
-  //  .then((result) => createElement(result.data))
-  //  .catch((error) => console.log(error.message))
-//};
-//getRandomThen();
+
+
+
+
+function clear () {
+  const delet = document.querySelectorAll('.content')[0];  
+  delet.innerHTML = '';
+}
